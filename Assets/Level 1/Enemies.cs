c9b527426ed4c775d62e6074c113729cfa2eadde
Caden Sampsel <caden.sampsel@icloud.com>
2024-10-01 08:09:59 -0700

    Test function for presentation

---
+++ Assets/Level 1/Enemies.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour {
+    
+    public void EnemyPathing() {
+        console.log("Enemy pathing feature coming soon");
+    }
+}
