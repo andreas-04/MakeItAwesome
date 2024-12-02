7adb5c1761c65214f61045fa7221738d76164de2
Andreas <andreasneacsu@gmail.com>
2024-12-02 14:20:48 -0800

    finish worm

--- Assets/Level 1/Scripts/Andreas/LevelLoader.cs
+++ Assets/Level 1/Scripts/Andreas/LevelLoader.cs
@@ -5,23 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    private float survivalTime = 60f;
-    private float startTime;
     private bool hasSurvived = false;
     private int currentLevel = 0;
 
     private void Start()
     {
-        startTime = Time.time;
         currentLevel = SceneManager.GetActiveScene().buildIndex;
     }
 
     private void Update()
     {
-        if (!hasSurvived && Time.time - startTime >= survivalTime && currentLevel == 2)
-        {
-            LoadNextLevel();
-        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -37,5 +31,6 @@ public class LevelLoader : MonoBehaviour
     {
         int nextLevelIndex = currentLevel + 1;
         SceneManager.LoadScene(nextLevelIndex);
+        Debug.Log("here");
     }
 }
